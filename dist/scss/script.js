5ed490ab4c7715019ac473946809f33e7175d93c
Nyleco-J <anuran.nyleco24@gmail.com>
2023-03-22 21:09:40 +0800

    wf

--- dist/scss/script.js
+++ dist/scss/script.js
@@ -1,6 +1,17 @@
-const burger =document.querySelector(".toggle__menu");
-const header =document.querySelector("header__nav");
+const burger = document.querySelector(".toggle__menu");
+const header = document.querySelector(".header__nav");
 burger.addEventListener("click", () =>{
-  burger.classList.toggle("open");
-  header.classList.toggle("open");
+  burger.classList.toggle('open');
+  header.classList.toggle('open');
+  dot.classList.remove('open');
+  time.classList.remove('open');
+});
+
+const dot = document.querySelector(".toggle__menu1");
+const time = document.querySelector(".info")
+dot.addEventListener("click", ()=>{
+  dot.classList.toggle('open');
+  time.classList.toggle('open');
+  burger.classList.remove('open');
+  header.classList.remove('open');
 })
