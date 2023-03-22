53e1dcdf595e56ee6304e005d8ccdb124057315e
Nyleco-J <anuran.nyleco24@gmail.com>
2023-03-22 15:42:03 +0800

    wonder

--- dist/scss/script.js
+++ dist/scss/script.js
@@ -1,27 +1,6 @@
-const burgerBtn = document.querySelector(".header__bars i");
-const headerNav = document.querySelector(".header__nav");
-
-const onClickBurgerBtn = (e) => {
-  headerNav.classList.toggle("active");
-};
-
-burgerBtn.addEventListener("click", onClickBurgerBtn);
-
-let slider = tns({
-  container: ".socials__wrapper",
-  mouseDrag: true,
-  items: 1,
-  gutter: 20,
-  controlsText: [
-    '<i class="fa-solid fa-chevron-left"></i>',
-    '<i class="fa-solid fa-chevron-right"></i>',
-  ],
-  slideBy: "page",
-  autoPlay: false,
-  axis: "horizontal",
-  responsive: {
-    1020: {
-      items: 3,
-    },
-  },
-});
+const burger =document.querySelector(".toggle__menu");
+const header =document.querySelector("header__nav");
+burger.addEventListener("click", () =>{
+  burger.classList.toggle("open");
+  header.classList.toggle("open");
+})
